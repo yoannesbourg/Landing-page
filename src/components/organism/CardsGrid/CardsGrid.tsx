532855b55b0e627344ad76cf60d9e806bd394db6
Yoann <yoannpascalbouix@gmail.com>
2021-11-12 13:20:28 +0100

    Favicon + refactor uuid

--- src/components/organism/CardsGrid/CardsGrid.tsx
+++ src/components/organism/CardsGrid/CardsGrid.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { CardsGridProps } from './CardsGridProps';
 import Card from '../../molecules/Card/Card';
+import { v4 as uuidv4 } from 'uuid';
+
 import './CardsGrid.scss';
 
 const CardsGrid = (props: CardsGridProps): JSX.Element => {
     const { cards } = props;
     return (
         <div className="cards-grid">
-            {cards.map((card, i) => {
-                return <Card key={i} {...card} />;
+            {cards.map((card) => {
+                return <Card key={uuidv4()} {...card} />;
             })}
         </div>
     );
